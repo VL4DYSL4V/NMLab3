ceee6c80a8ab91c3c4732678417677e8d6e1afbc
VladislavArzamastsev <arzik66677@gmail.com>
2021-10-23 15:23:12 +0300

    Updated properties, state and pom.xml

--- src/main/java/state/ApplicationState.java
+++ src/main/java/state/ApplicationState.java
@@ -1,6 +1,35 @@
 package state;
 
 import framework.state.AbstractApplicationState;
+import framework.state.StateHelper;
+import framework.utils.ConsoleUtils;
+import lombok.Getter;
 
+@Getter
 public class ApplicationState extends AbstractApplicationState {
+
+    private final String system = "\n  /\u203E sin(x - 0.6) - y = 1.6\n"
+                                +"<\n"
+                                +"  \\_3x - cos(y) = 0.9";
+
+    private double precision = 0.00001;
+
+    @Override
+    protected void initVariableNameToSettersMap() {
+        variableNameToSetter.put("precision", StateHelper
+                .getDefaultSetter("precision", Double.class, o -> (Double) o, this::setPrecision));
+    }
+
+    @Override
+    protected void initVariableNameToGettersMap() {
+        variableNameToGetter.put("precision", this::getPrecision);
+        variableNameToGetter.put("system", this::getSystem);
+    }
+
+    public void setPrecision(double precision) {
+        if (precision < 0) {
+            ConsoleUtils.println(variableHolder.getVariable("precision").getConstraintViolationMessage());
+        }
+        this.precision = precision;
+    }
 }
